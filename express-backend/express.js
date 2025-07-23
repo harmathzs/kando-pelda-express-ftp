da6b7a028b8961f51c583e5bd81919cf645c54b5
Zsolt Harmath <harmathzs@gmail.com>
2025-07-23 15:14:58 +0200

    Retrieve a file on backend

--- express-backend/express.js
+++ express-backend/express.js
@@ -1,3 +1,4 @@
+const ftp = require('basic-ftp');
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,45 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/ftp-file/:name', async (req, res) => {
+  //console.log('req', req);
+  const name = req.params.name;
+  const filename = '/' + name + '.txt';
+
+const client = new ftp.Client();
+  client.ftp.verbose = true;
+
+  try {
+    await client.access({
+      host: "127.0.0.1",
+      port: 21,
+      user: "user",
+      password: "pass",
+      secure: false
+    });
+
+    const chunks = [];
+    await client.downloadTo(
+      new require("stream").Writable({
+        write(chunk, _, callback) {
+          chunks.push(chunk);
+          callback();
+        }
+      }),
+      filename
+    );
+
+    const result = Buffer.concat(chunks).toString("utf-8");
+    res.status(200).send(result);
+
+  } catch (err) {
+    console.warn("FTP Error:", err.message);
+    res.status(500).send("FTP error: " + err.message);
+  } finally {
+    client.close();
+  }
 });
 
 const port = 3333;
